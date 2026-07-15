b3f550cdbff11848f6bf35ca8d8de42737ffbe44
VeneratedApotheosis <5432154321abcabcabcabcarahesdh@gmail.com>
2026-07-16 03:37:36 +0800

    test fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/Alan-Luu-Website-Pages",
+  basePath: "/Alan-Luu-Website-Pages/",
   output: "export",
   reactCompiler: true,
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
